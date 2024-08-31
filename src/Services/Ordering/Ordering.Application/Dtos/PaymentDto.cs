c2c8114aeb95cc95df79c93f90ffa4e2042e8c20
thiago-am-souza <thiagosouza96.ts@gmail.com>
2024-08-31 16:42:07 -0300

    orders commands and queries

---
+++ src/Services/Ordering/Ordering.Application/Dtos/PaymentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Dtos
+{
+    public record PaymentDto(
+        string CardName,
+        string CarNumber,
+        string Expiration,
+        string Cvv,
+        int PaymentMethod);
+}
